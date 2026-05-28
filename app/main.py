875f7fe682575bda1bd144db098d5cf5e7d41415
DrunkPooh <top03010@gmail.com>
2026-05-28 10:32:58 +0900

    style: ruff format 적용 (app/main.py)

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ from fastapi import FastAPI
 from app.apis.v1 import v1_routers
 from app.core.db.databases import initialize_tortoise
 
-app = FastAPI(
-    docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json"
-)
+app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json")
 
 initialize_tortoise(app)
 
